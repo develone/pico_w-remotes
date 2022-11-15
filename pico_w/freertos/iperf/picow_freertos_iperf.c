b0ca0ec84a83d97c29766f32c8ff243dec374d2a
Ed Vidal Jr <develone@sbcglobal.net>
2022-11-15 15:15:03 -0700

    adding a new task from hello_7segment.c gpio example

--- pico_w/freertos/iperf/picow_freertos_iperf.c
+++ pico_w/freertos/iperf/picow_freertos_iperf.c
@@ -6,6 +6,10 @@
 
 #include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
+/*needed for GPIO from pico-examples/gpio/hello_7segment/hello_7segment.c
+gpio will be an additional freertos task
+*/
+#include "hardware/gpio.h"
 
 #include "lwip/netif.h"
 #include "lwip/ip4_addr.h"
@@ -21,16 +25,36 @@
 #include "xtransmit.h"
 #include "xreceive.h"
 
+/*needed for GPIO from pico-examples/gpio/hello_7segment/hello_7segment.c
+gpio will be an additional freertos task
+*/
+#define FIRST_GPIO 2
+#define BUTTON_GPIO (FIRST_GPIO+7)
+
 #ifndef RUN_FREERTOS_ON_CORE
 #define RUN_FREERTOS_ON_CORE 0
 #endif
 
 #define TEST_TASK_PRIORITY				( tskIDLE_PRIORITY + 2UL )
 #define BLINK_TASK_PRIORITY				( tskIDLE_PRIORITY + 1UL )
-
+#define GPIO_TASK_PRIORITY				( tskIDLE_PRIORITY + 3UL )
 #if CLIENT_TEST && !defined(IPERF_SERVER_IP)
 #error IPERF_SERVER_IP not defined
 #endif
+
+// This array converts a number 0-9 to a bit pattern to send to the GPIOs
+int bits[10] = {
+        0x3f,  // 0
+        0x06,  // 1
+        0x5b,  // 2
+        0x4f,  // 3
+        0x66,  // 4
+        0x6d,  // 5
+        0x7d,  // 6
+        0x07,  // 7
+        0x7f,  // 8
+        0x67   // 9
+};
 
 // Report IP results and exit
 static void iperf_report(void *arg, enum lwiperf_report_type report_type,
@@ -63,6 +87,67 @@ void blink_task(__unused void *params) {
     }
 }
 
+void gpio_task(__unused void *params) {
+    //bool on = false;
+    printf("gpio_task starts\n");
+    while (true) {
+#if 0 && configNUM_CORES > 1
+        static int last_core_id;
+        if (portGET_CORE_ID() != last_core_id) {
+            last_core_id = portGET_CORE_ID();
+            printf("blinking now from core %d\n", last_core_id);
+        }
+#endif
+        //cyw43_arch_gpio_put(0, on);
+        //on = !on;
+        
+// We could use gpio_set_dir_out_masked() here
+    for (int gpio = FIRST_GPIO; gpio < FIRST_GPIO + 7; gpio++) {
+        gpio_init(gpio);
+        gpio_set_dir(gpio, GPIO_OUT);
+        // Our bitmap above has a bit set where we need an LED on, BUT, we are pulling low to light
+        // so invert our output
+        gpio_set_outover(gpio, GPIO_OVERRIDE_INVERT);
+    }
+
+    gpio_init(BUTTON_GPIO);
+    gpio_set_dir(BUTTON_GPIO, GPIO_IN);
+    // We are using the button to pull down to 0v when pressed, so ensure that when
+    // unpressed, it uses internal pull ups. Otherwise when unpressed, the input will
+    // be floating.
+    gpio_pull_up(BUTTON_GPIO);
+
+    int val = 0;
+    while (true) {
+        // Count upwards or downwards depending on button input
+        // We are pulling down on switch active, so invert the get to make
+        // a press count downwards
+        if (!gpio_get(BUTTON_GPIO)) {
+            if (val == 9) {
+                val = 0;
+            } else {
+                val++;
+            }
+        } else if (val == 0) {
+            val = 9;
+        } else {
+            val--;
+        }
+
+        // We are starting with GPIO 2, our bitmap starts at bit 0 so shift to start at 2.
+        int32_t mask = bits[val] << FIRST_GPIO;
+
+        // Set all our GPIOs in one go!
+        // If something else is using GPIO, we might want to use gpio_put_masked()
+        gpio_set_mask(mask);
+        sleep_ms(250);
+        gpio_clr_mask(mask);
+    }
+        
+        vTaskDelay(200);
+    }
+}
+
 void main_task(__unused void *params) {
     if (cyw43_arch_init()) {
         printf("failed to initialise\n");
@@ -79,6 +164,7 @@ void main_task(__unused void *params) {
 
     xTaskCreate(blink_task, "BlinkThread", configMINIMAL_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
 
+    xTaskCreate(gpio_task, "GPIOThread", configMINIMAL_STACK_SIZE, NULL, GPIO_TASK_PRIORITY, NULL);
 #if CLIENT_TEST
     printf("\nReady, running iperf client\n");
     ip_addr_t clientaddr;
@@ -115,6 +201,7 @@ void vLaunch( void) {
 int main( void )
 {
     stdio_init_all();
+    printf("adding gpio support to the program\n");
 	unsigned char message[3] = {0xd3, 0x01, 0x00};
 	buildCRCTable();
 	printf("Back from buildCRCTable \n");
