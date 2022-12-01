2fec989d244106733fee09ec3a8b0d4b5cb182ee
Edward Vidal Jr <develone@sbcglobal.net>
2022-11-30 17:42:36 -0700

    missing remove remote6

--- 6remotes.sh
+++ 6remotes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rf remote1  remote2 remote3 remote4 remote5
+rm -rf remote1  remote2 remote3 remote4 remote5 remote6
 
 mkdir remote1
 
@@ -15,23 +15,23 @@ mkdir remote6
 
 cd remote1
 
-cmake -DHOSTNAME="remote1"  -DPICO_BOARD=pico_w  -DTEST_TCP_SERVER_IP="10.0.1.12" -DWIFI_SSID="nanotest" -DWIFI_PASSWORD="t?bqxvcqh#6t" -DFREERTOS_KERNEL_PATH:PATH=/home/devel/FreeRTOS-Kernel ..
+cmake -DHOSTNAME="remote1"  -DPICO_BOARD=pico_w  -DTEST_TCP_SERVER_IP="10.0.1.12" -DWIFI_SSID="nanotest" -DWIFI_PASSWORD="12345678" -DFREERTOS_KERNEL_PATH:PATH=/home/devel/FreeRTOS-Kernel ..
 
 cd ../remote2
 
-cmake -DHOSTNAME="remote2"  -DPICO_BOARD=pico_w  -DTEST_TCP_SERVER_IP="10.0.1.12" -DWIFI_SSID="nanotest" -DWIFI_PASSWORD="t?bqxvcqh#6t" -DFREERTOS_KERNEL_PATH:PATH=/home/devel/FreeRTOS-Kernel ..
+cmake -DHOSTNAME="remote2"  -DPICO_BOARD=pico_w  -DTEST_TCP_SERVER_IP="10.0.1.12" -DWIFI_SSID="nanotest" -DWIFI_PASSWORD="12345678" -DFREERTOS_KERNEL_PATH:PATH=/home/devel/FreeRTOS-Kernel ..
 
 cd ../remote3
 
-cmake -DHOSTNAME="remote3"   -DPICO_BOARD=pico_w  -DTEST_TCP_SERVER_IP="10.0.1.12" -DWIFI_SSID="nanotest" -DWIFI_PASSWORD="t?bqxvcqh#6t" -DFREERTOS_KERNEL_PATH:PATH=/home/devel/FreeRTOS-Kernel ..
+cmake -DHOSTNAME="remote3"   -DPICO_BOARD=pico_w  -DTEST_TCP_SERVER_IP="10.0.1.12" -DWIFI_SSID="nanotest" -DWIFI_PASSWORD="12345678" -DFREERTOS_KERNEL_PATH:PATH=/home/devel/FreeRTOS-Kernel ..
 
 cd ../remote4
 
-cmake -DHOSTNAME="remote4"  -DPICO_BOARD=pico_w  -DTEST_TCP_SERVER_IP="10.0.1.12" -DWIFI_SSID="nanotest" -DWIFI_PASSWORD="t?bqxvcqh#6t" -DFREERTOS_KERNEL_PATH:PATH=/home/devel/FreeRTOS-Kernel ..
+cmake -DHOSTNAME="remote4"  -DPICO_BOARD=pico_w  -DTEST_TCP_SERVER_IP="10.0.1.12" -DWIFI_SSID="nanotest" -DWIFI_PASSWORD="12345678" -DFREERTOS_KERNEL_PATH:PATH=/home/devel/FreeRTOS-Kernel ..
 
 cd ../remote5
 
-cmake -DHOSTNAME="remote5"  -DPICO_BOARD=pico_w  -DTEST_TCP_SERVER_IP="10.0.1.12" -DWIFI_SSID="nanotest" -DWIFI_PASSWORD="t?bqxvcqh#6t" -DFREERTOS_KERNEL_PATH:PATH=/home/devel/FreeRTOS-Kernel ..
+cmake -DHOSTNAME="remote5"  -DPICO_BOARD=pico_w  -DTEST_TCP_SERVER_IP="10.0.1.12" -DWIFI_SSID="nanotest" -DWIFI_PASSWORD="12345678" -DFREERTOS_KERNEL_PATH:PATH=/home/devel/FreeRTOS-Kernel ..
 
 cd ../remote6
 
