57a1868a4766084b9c5b6908cb8a167ed34dd713
Edward Vidal Jr <develone@sbcglobal.net>
2022-12-04 10:35:20 -0700

    These past updates provide rtc and ntp
    adding gpio support to the program
    Back from buildCRCTable
    0xd3 0x1 0x2e
    Starting FreeRTOS on core 0:
    Connecting to WiFi...
    Connected.
    mqtt_port = 1883 &mqtt_port 0x200005cc
    mqtt_ip = 0xe501a8c0 &mqtt_ip = 0x200005c8
    IPADDR_LOOPBACK = 0x7f000001
    mqtt_client 0x200227f8 &mqtt_client 0x200017fc
    mqtt_set_inpub_callback 0x10001a65
    mqtt_client_connect 0x10001a7d
    0x1001a7d8
    this is a message from pico_w   31
    gpio_task starts
    mqtt_task starts
    ntp_task starts
    
    Ready, running iperf server at 192.168.1.175
    blink_task starts
    MQTT client "remote6" connection cb: status 0
    MQTT client "remote6" request cb: err 0
    MQTT client "remote6" request cb: err 0
    ntp address 108.61.73.244
    got ntp response: 04/12/2022 02:47:53
    got ntp response: 04/12/2022 02:48:23
    got ntp response: 04/12/2022 02:48:54
    got ntp response: 04/12/2022 02:49:25
    ntp address 17.253.2.123
    got ntp response: 04/12/2022 02:49:56
    MQTT client "remote6" connection cb: status 256
    ntp address 198.137.202.56
    got ntp response: 04/12/2022 02:50:27
    RTC Alarm!
    adding gpio support to the program
    Back from buildCRCTable
    0xd3 0x1 0x2e
    Starting FreeRTOS on core 0:
    Connecting to WiFi...
    Connected.
    mqtt_port = 1883 &mqtt_port 0x200005cc
    mqtt_ip = 0xe501a8c0 &mqtt_ip = 0x200005c8
    IPADDR_LOOPBACK = 0x7f000001
    mqtt_client 0x20022800 &mqtt_client 0x20001800
    mqtt_set_inpub_callback 0x10001ad1
    mqtt_client_connect 0x10001ae9
    0x1001ac08
    this is a message from pico_w   31
    gpio_task starts
    mqtt_task starts
    ntp_task starts
    
    Ready, running iperf server at 192.168.1.175
    blink_task starts
    MQTT client "remote6" connection cb: status 0
    MQTT client "remote6" request cb: err 0
    MQTT client "remote6" request cb: err 0
    ntp address 66.220.9.122
    got ntp response: 04/12/2022 17:25:00
    got ntp response: 04/12/2022 17:25:30
    ntp address 72.30.35.89
    got ntp response: 04/12/2022 17:26:01
    got ntp response: 04/12/2022 17:26:32
    Alarm Fired At Wednesday 13 January 11:22:05 2020

--- pico_w/freertos/mqtt/mqtt_example.c
+++ pico_w/freertos/mqtt/mqtt_example.c
@@ -26,8 +26,24 @@
  * Author: Dirk Ziegelmeier <dziegel@gmx.de>
  *
  */
+#include <stdio.h> 
+//#include "hardware/rtc.h"
 #include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
+
+#include "pico/util/datetime.h"
+static volatile bool fired = false;
+
+static void alarm_callback(void) {
+    datetime_t t = {0};
+    rtc_get_datetime(&t);
+    char datetime_buf[256];
+    char *datetime_str = &datetime_buf[0];
+    datetime_to_str(datetime_str, sizeof(datetime_buf), &t);
+    printf("Alarm Fired At %s\n", datetime_str);
+    stdio_flush();
+    fired = true;
+}
 
 /*needed for ntp*/
 #include <string.h>
@@ -561,6 +577,36 @@ void run_ntp_test(void) {
 /*needed for ntp*/
 int main() {
     stdio_init_all();
+    printf("RTC Alarm!\n");
+
+    // Start on Wednesday 13th January 2021 11:20:00
+    datetime_t t = {
+        .year  = 2020,
+        .month = 01,
+        .day   = 13,
+        .dotw  = 3, // 0 is Sunday, so 3 is Wednesday
+        .hour  = 11,
+        .min   = 20,
+        .sec   = 00
+    };
+
+    // Start the RTC
+    rtc_init();
+    rtc_set_datetime(&t);
+
+    // Alarm 5 seconds later
+    datetime_t alarm = {
+        .year  = 2020,
+        .month = 01,
+        .day   = 13,
+        .dotw  = 3, // 0 is Sunday, so 3 is Wednesday
+        .hour  = 11,
+        .min   = 22,
+        .sec   = 05
+    };
+
+    rtc_set_alarm(&alarm, &alarm_callback);
+
     printf("adding gpio support to the program\n");
 	unsigned char message[3] = {0xd3, 0x01, 0x00};
 	buildCRCTable();
