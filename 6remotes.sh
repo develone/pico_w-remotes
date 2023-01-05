eade9f0fc078a0ae87f74ec2fd2d032cfdb29085
Ed Vidal Jr <develone@sbcglobal,net>
2023-01-05 14:46:11 -0700

    now uses pico_w/freertos/mqtt/pw_ssid.h

--- 6remotes.sh
+++ 6remotes.sh
@@ -15,27 +15,28 @@ mkdir remote6
 
 cd remote1
 
-cmake -DHOSTNAME="remote1"  -DPICO_BOARD=pico_w  -DTEST_TCP_SERVER_IP="10.0.1.12"  -DFREERTOS_KERNEL_PATH:PATH=/home/devel/FreeRTOS-Kernel ..
+cmake -DHOSTNAME="remote1"  -DPICO_BOARD=pico_w   -DFREERTOS_KERNEL_PATH:PATH=/home/devel/FreeRTOS-Kernel ..
 
 cd ../remote2
 
-cmake -DHOSTNAME="remote2"  -DPICO_BOARD=pico_w  -DTEST_TCP_SERVER_IP="10.0.1.12"  -DFREERTOS_KERNEL_PATH:PATH=/home/devel/FreeRTOS-Kernel ..
+cmake -DHOSTNAME="remote2"  -DPICO_BOARD=pico_w   -DFREERTOS_KERNEL_PATH:PATH=/home/devel/FreeRTOS-Kernel .. 
 
 cd ../remote3
 
-cmake -DHOSTNAME="remote3"   -DPICO_BOARD=pico_w  -DTEST_TCP_SERVER_IP="10.0.1.12"  -DFREERTOS_KERNEL_PATH:PATH=/home/devel/FreeRTOS-Kernel ..
+cmake -DHOSTNAME="remote3"  -DPICO_BOARD=pico_w   -DFREERTOS_KERNEL_PATH:PATH=/home/devel/FreeRTOS-Kernel ..
 
 cd ../remote4
 
-cmake -DHOSTNAME="remote4"  -DPICO_BOARD=pico_w  -DTEST_TCP_SERVER_IP="10.0.1.12"  -DFREERTOS_KERNEL_PATH:PATH=/home/devel/FreeRTOS-Kernel ..
+cmake -DHOSTNAME="remote4"  -DPICO_BOARD=pico_w   -DFREERTOS_KERNEL_PATH:PATH=/home/devel/FreeRTOS-Kernel .. 
+cd ../remote1
 
 cd ../remote5
 
-cmake -DHOSTNAME="remote5"  -DPICO_BOARD=pico_w  -DTEST_TCP_SERVER_IP="10.0.1.12"  -DFREERTOS_KERNEL_PATH:PATH=/home/devel/FreeRTOS-Kernel ..
+cmake -DHOSTNAME="remote5"  -DPICO_BOARD=pico_w   -DFREERTOS_KERNEL_PATH:PATH=/home/devel/FreeRTOS-Kernel ..
 
 cd ../remote6
 
-cmake -DHOSTNAME="remote6"  -DPICO_BOARD=pico_w  -DTEST_TCP_SERVER_IP="10.0.1.12"  -DFREERTOS_KERNEL_PATH:PATH=/home/devel/FreeRTOS-Kernel ..
+cmake -DHOSTNAME="remote6"  -DPICO_BOARD=pico_w   -DFREERTOS_KERNEL_PATH:PATH=/home/devel/FreeRTOS-Kernel .. 
 
 cd ../remote1
 
@@ -43,24 +44,18 @@ make &
 
 cd ../remote2
 
-make &
-
+make  &
 
 cd ../remote3
 
 make &
-
 cd ../remote4
 
-make &
-
+make  &
 
 cd ../remote5
 
 make &
-
 cd ../remote6
 
-make &
-
-
+make  &
